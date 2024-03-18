ceb7ce72ba21228b6ddbd45d672522e199c4af2c
altf4arnold <arnold.dech@adct.be>
2024-03-18 21:49:32 +0100

    pulls the departure data

--- datapull.py
+++ datapull.py
@@ -10,11 +10,12 @@ airport = 'EBBR'
 payload = {'max_pages': 2}
 auth_header = {'x-apikey':config.FLIGHTAWAREKEY}
 
-response = requests.get(apiUrl + f"airports/{airport}/flights",
+response = requests.get(apiUrl + f"airports/{airport}/flights/scheduled_departures",
     params=payload, headers=auth_header)
 
 if response.status_code == 200:
-    print(response.json())
+    departures = response.json()
+    print(departures)
 else:
     print("Error executing request")
 
