f1676580ee0ae9f50eaeb0f884c47c6f33a979dc
altf4arnold <arnold.dech@adct.be>
2024-03-18 21:56:39 +0100

    Adding the airport ICAO code and the comments

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+"""
+Config file that contains var's for the program.
+"""
+# Your API key that you get from https://www.flightaware.com/commercial/aeroapi
 FLIGHTAWAREKEY = "V3rYs3cr3t"
+
+# ICAO code of the airport you want to see
+AIRPORT = "EBBR"
